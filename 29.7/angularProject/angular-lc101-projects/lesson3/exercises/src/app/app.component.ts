310c889b6a9aebbbd50efe3444ac4d2a3b4f04fb
Anthony Sega <94858432+Antsega@users.noreply.github.com>
2022-12-20 13:52:37 -0600

    Chap 31 Angular, Part 3 exercise completed

--- 29.7/angularProject/angular-lc101-projects/lesson3/exercises/src/app/app.component.ts
+++ 29.7/angularProject/angular-lc101-projects/lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,58 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  takeOffEnabled: boolean = true;
+
+  moveRocket(variable, direction) {
+    if (direction === 'right'){
+      let movement = parseInt(variable.style.left) + 10 + 'px';
+      variable.style.left = movement;
+      this.width += 10000;
+  }
+  if (direction === 'left'){
+    let movement = parseInt(variable.style.left) - 10 + 'px';
+    variable.style.left = movement;
+    this.width -= 10000;
+}
+if (direction === 'up'){
+  let movement = parseInt(variable.style.bottom) + 10 + 'px';
+  variable.style.bottom = movement;
+  this.height += 10000;
+}
+if (direction === 'down'){
+  let movement = parseInt(variable.style.bottom) - 10 + 'px';
+  variable.style.bottom = movement;
+  this.height += 10000;
+}
+  }
+
+  handleTakeOff() {
+    let results = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (results) {
+      this.color = 'blue';
+      this.height = 1000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+    }
+  }
+
+  handleLand() {
+    let results = window.confirm('Are you sure the shuttle is ready to land?');
+    if (results) {
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'The shuttle has landed.';
+    }
+  }
+  abort() {
+    let results = window.confirm('Are you sure you want to abort the mission?');
+    if (results) {
+      this.color = 'red';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'The shuttle has landed.';
+    }
+  }
 }
